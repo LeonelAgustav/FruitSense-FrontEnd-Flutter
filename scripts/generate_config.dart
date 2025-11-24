9006bbf6a6887bcf5cae3caf72d2281c4cd55ac2
Leonel <marcelinoagustav@gmail.com>
2025-11-24 12:17:22 +0700

    Update scripts/generate_config

--- scripts/generate_config.dart
+++ scripts/generate_config.dart
@@ -19,13 +19,11 @@ void main() {
   final Map<String, String> envVars = {};
 
   for (var line in envLines) {
-    // Abaikan komentar atau baris kosong
     if (line.trim().isEmpty || line.startsWith('#')) continue;
 
     final parts = line.split('=');
     if (parts.length >= 2) {
       final key = parts[0].trim();
-      // Menggabungkan kembali value jika ada tanda '=' di dalamnya (misal base64)
       final value = parts.sublist(1).join('=').trim();
       envVars[key] = value;
     }
@@ -35,6 +33,7 @@ void main() {
   final apiKey = envVars['FIREBASE_API_KEY'];
   final projectId = envVars['FIREBASE_PROJECT_ID'];
   final appId = envVars['FIREBASE_APP_ID'];
+  final projectNumber = envVars['FIREBASE_PROJECT_NUMBER'];
 
   if (apiKey == null || projectId == null || appId == null) {
     print(
@@ -44,11 +43,9 @@ void main() {
   }
 
   // 5. Buat struktur JSON sesuai template Anda
-  // Note: package_name diset sesuai request Anda, bisa juga ditaruh di .env jika mau dinamis
   final Map<String, dynamic> googleServicesJson = {
     "project_info": {
-      "project_number":
-          "848546376772", // Bisa diambil dari .env jika perlu dinamis
+      "project_number": projectNumber,
       "project_id": projectId,
       "storage_bucket": "$projectId.firebasestorage.app",
     },
